bf4db05ff10219a2af35f0f454eeea553e2240da
Marcos Monte Fidélis de Araújo <marcosmonfi@gmail.com>
2022-12-26 11:31:50 -0500

    estou movendo codigo. game vai conter todos os metodos referentes ao blackjack em si e ao contato com o jogador, introduzi um teste. criei erros no main porque vai requerer reescrever algumas coisinhas pra fazer as coisas funcionarem mais acopladas

--- src/baralho.rs
+++ src/baralho.rs
@@ -1,9 +1,9 @@
 use core::fmt::{Display, Formatter};
-use rand::prelude::*;
-use thiserror::Error;
+use rand::prelude::SliceRandom;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
-
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, EnumIter)]
 pub enum NAIPE {
     Paus,
     Copas,
@@ -22,17 +22,10 @@ impl Display for NAIPE {
     }
 }
 
-#[derive(Debug, Error)]
-#[non_exhaustive]
-pub enum CartaError {
-    #[error("Suit not one of 4 suits")]
-    InvalidSuit,
-}
-
 #[derive(Debug, Clone)]
 pub struct Carta {
-    pub naipe: NAIPE,
-    pub valor: String,
+    naipe: NAIPE,
+    valor: String,
 }
 
 impl Carta {
@@ -47,14 +40,6 @@ impl Carta {
 
     pub fn parsed_valor(&self) -> u32 {
         self.valor.parse::<u32>().unwrap()
-    }
-
-    pub fn new_ctrl(n: NAIPE, v: String) -> Result<Carta, CartaError> {
-            let saida = match n {
-                NAIPE => Ok(Carta::new(n, v)),
-                _=> Err(CartaError::InvalidSuit),
-            };
-            saida
     }
 }
 
@@ -80,44 +65,16 @@ impl Baralho {
         s
     }
     pub fn gerar(&mut self) {
-        self.deck.push(Carta::new(NAIPE::Paus, String::from("As")));
-        for i in 2..11 {
-            self.deck.push(Carta::new(NAIPE::Paus, i.to_string()));
+        for n in NAIPE::iter() {
+            self.deck.push(Carta::new(n, String::from("As")));
+            for i in 2..11 {
+                self.deck.push(Carta::new(n, i.to_string()));
+            }
+            self.deck.push(Carta::new(n, String::from("J")));
+            self.deck.push(Carta::new(n, String::from("Q")));
+            self.deck.push(Carta::new(n, String::from("K")));
         }
-        self.deck.push(Carta::new(NAIPE::Paus, String::from("J")));
-        self.deck.push(Carta::new(NAIPE::Paus, String::from("Q")));
-        self.deck.push(Carta::new(NAIPE::Paus, String::from("K")));
-
-        self.deck.push(Carta::new(NAIPE::Copas, String::from("As")));
-        for i in 2..11 {
-            self.deck.push(Carta::new(NAIPE::Copas, i.to_string()));
-        }
-        self.deck.push(Carta::new(NAIPE::Copas, String::from("J")));
-        self.deck.push(Carta::new(NAIPE::Copas, String::from("Q")));
-        self.deck.push(Carta::new(NAIPE::Copas, String::from("K")));
-
-        self.deck
-            .push(Carta::new(NAIPE::Espada, String::from("As")));
-        for i in 2..11 {
-            self.deck.push(Carta::new(NAIPE::Espada, i.to_string()));
-        }
-        self.deck.push(Carta::new(NAIPE::Espada, String::from("J")));
-        self.deck.push(Carta::new(NAIPE::Espada, String::from("Q")));
-        self.deck.push(Carta::new(NAIPE::Espada, String::from("K")));
-
-        self.deck.push(Carta::new(NAIPE::Ouros, String::from("As")));
-        for i in 2..11 {
-            self.deck.push(Carta::new(NAIPE::Ouros, i.to_string()));
-        }
-        self.deck.push(Carta::new(NAIPE::Ouros, String::from("J")));
-        self.deck.push(Carta::new(NAIPE::Ouros, String::from("Q")));
-        self.deck.push(Carta::new(NAIPE::Ouros, String::from("K")));
     }
-
-    // fn adicionar_baralho(&mut self,n:u32) {
-    // let bara_adic = Baralho::new(n);
-    // bara_adic.gerar();
-    // }
 
     pub fn baralhos_totais(&mut self, n: u32) {
         for i in 0..n {
@@ -130,5 +87,17 @@ impl Baralho {
     pub fn embaralhar(&mut self) {
         let mut rng = rand::thread_rng();
         self.deck.shuffle(&mut rng);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn tamanho_certo() {
+        let mut b = Baralho::new(0);
+        b.gerar();
+        assert_eq!(52, b.deck.len());
     }
 }
--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,6 @@
 // mod baralho;
 use crate::baralho::*;
+use thiserror::Error;
 // use std::io;
 // use thiserror::Error;
 
@@ -10,49 +11,57 @@ use crate::baralho::*;
 //     InvalidSuit,
 // }
 
-pub fn contagem(h: Vec<Carta>) -> u32 {
-    let mut s: u32 = 0;
-    let mut wait: u32 = 0;
-    for c in h {
-        match c.get_valor() {
-            "j" | "J" | "q" | "Q" | "k" | "K" => s += 10,
-            "As" => wait += 1,
-            _ => s += c.parsed_valor(),
-        }
-    }
-    while wait > 0 {
-        let teste = s + (wait * 10);
-        if teste > 21 {
-            s += 1;
-            wait -= 1;
-        } else {
-            s += 10;
-            wait -= 1;
-        }
-    }
-    s
+pub struct Blackjack {
+    dealer: Baralho,
+    jgdr: Vec<Carta>,
+    mesa: Vec<Carta>,
 }
 
-pub fn mesa(valor_mao: u32, b: &mut Baralho) {
-    let mut table: Vec<Carta> = Vec::new();
-    loop {
-        let valor_mesa = contagem(table.clone());
-        table.push(b.deck.pop().unwrap());
-        if valor_mesa > 21 {
-            println!("{} House Bust", valor_mesa);
-            break;
-        // } else if contagem(hand.clone()) > contagem(table.clone()) && contagem(table.clone()) < 21 {
-        //     println!("House Loses");
-        //     break;
-        } else if valor_mao < valor_mesa && valor_mesa <= 21 {
-            println!("{} House Wins", valor_mesa);
-            break;
+impl Blackjack {
+    pub fn contagem(h: Vec<Carta>) -> u32 {
+        let mut s: u32 = 0;
+        let mut wait: u32 = 0;
+        for c in h {
+            match c.get_valor() {
+                "j" | "J" | "q" | "Q" | "k" | "K" => s += 10,
+                "As" => wait += 1,
+                _ => s += c.parsed_valor(),
+            }
+        }
+        while wait > 0 {
+            let teste = s + (wait * 10);
+            if teste > 21 {
+                s += 1;
+                wait -= 1;
+            } else {
+                s += 10;
+                wait -= 1;
+            }
+        }
+        s
+    }
+
+    pub fn mesa(valor_mao: u32, b: &mut Baralho) {
+        let mut table: Vec<Carta> = Vec::new();
+        loop {
+            let valor_mesa = Blackjack::contagem(table.clone());
+            table.push(b.deck.pop().unwrap());
+            if valor_mesa > 21 {
+                println!("{} House Bust", valor_mesa);
+                break;
+            // } else if contagem(hand.clone()) > contagem(table.clone()) && contagem(table.clone()) < 21 {
+            //     println!("House Loses");
+            //     break;
+            } else if valor_mao < valor_mesa && valor_mesa <= 21 {
+                println!("{} House Wins", valor_mesa);
+                break;
+            }
         }
     }
-}
 
-pub fn hit(hand: &mut Vec<Carta>, b: &mut Baralho) {
-    let temp = b.deck.pop().unwrap();
-    println!("carta pra mao: {:?}", temp);
-    hand.push(temp);
+    pub fn hit(hand: &mut Vec<Carta>, b: &mut Baralho) {
+        let temp = b.deck.pop().unwrap();
+        println!("carta pra mao: {:?}", temp);
+        hand.push(temp);
+    }
 }
